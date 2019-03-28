280e9cab5e8a8a4ca50caafec0b0809593847352
AlmuHS <liberamenso10000@gmail.com>
2019-03-28 17:13:11 +0100

    added calculator object

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <Calculator.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Calculator calc;
+
+    std::cout<<calc.sum(1,2);
+
     return 0;
 }
